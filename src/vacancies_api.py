01e94814274bd1e354fd5d7802d7e51e9d907a25
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-11-04 05:21:01 +0300

    Реализованы классы для получения вакансий

---
+++ src/vacancies_api.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+import requests
+import os
+
+
+class VacanciesApi(ABC):
+    """Абстрактный класс для работы с API"""
+    @abstractmethod
+    def get_vacancies(self, name_vacancies):
+        pass
+
+
+class HeadHunterAPI(VacanciesApi):
+    """Класс для получения вакансий с hh.ru"""
+    def get_vacancies(self, name_vacancies):
+        """Метод для получения вакансий.
+           Возвращает вакансии в которых указано ключевое слово name_vacancies.
+           Возвращает максимум 50 вакансий"""
+        vacancies = requests.get(f"https://api.hh.ru/vacancies?"
+                                 f"per_page=50&text={name_vacancies}")
+
+        return vacancies.json()["items"]
+
+
+class SuperJobAPI(VacanciesApi):
+    """Класс для получения вакансий с superjob.ru"""
+    def get_vacancies(self, name_vacancies):
+        """Метод для получения вакансий.
+           Возвращает вакансии в которых указано ключевое слово name_vacancies.
+           Возвращает максимум 50 вакансий"""
+        api_key = os.getenv('Superjob_API')
+
+        vacancies = requests.get(f"https://api.superjob.ru/2.0/{api_key}/vacancies?"
+                                 f"count=50&keyword={name_vacancies}")
+
+        return vacancies.json()["objects"]

9cdfdc783e06c8a41cead6e4fc0a89d12d893475
Ilya Kalberdin <ilyakalberdin@gmail.com>
2023-11-04 21:25:24 +0300

    Переписан метод get_vacancy и изменены названия переменных

--- src/classes_to_save.py
+++ src/classes_to_save.py
@@ -6,54 +6,50 @@ import json
 class SaveToFile(ABC):
     """Абстрактный класс для работы с файлом"""
     @abstractmethod
-    def save_data(self, data: list) -> None:
+    def save_vacancies(self, vacancies: list) -> None:
         """Метод для сохранения данных в файл"""
         pass
 
     @abstractmethod
-    def get_data(self, param: str) -> list:
+    def get_vacancy(self, name_vacancy: str) -> list:
         """Метод для получения данных из файла по параметрам"""
         pass
 
     @abstractmethod
-    def delete_data(self) -> None:
+    def delete_vacancies(self) -> None:
         """Метод для удаления данных из файла"""
         pass
 
 
 class SaveToFileJSON(SaveToFile):
     """Класс для работы с json файлом """
-    def save_data(self, data: list) -> None:
+    def save_vacancies(self, vacancies: list) -> None:
         """Метод для сохранения данных в json файл"""
         with open(os.path.join("../", "data", "data.json"), "w", encoding="utf-8") as file:
             all_data = []
 
-            for d in data:
-                all_data.append({"name": d.name,
-                                 "salary_from": d.salary_from,
-                                 "salary_to": d.salary_to,
-                                 "currency": d.currency,
-                                 "area": d.area,
-                                 "url": d.url,
-                                 "description": d.description,
-                                 "requirements": d.requirements})
+            for vacancy in vacancies:
+                all_data.append({"name": vacancy.name,
+                                 "salary_from": vacancy.salary_from,
+                                 "salary_to": vacancy.salary_to,
+                                 "currency": vacancy.currency,
+                                 "area": vacancy.area,
+                                 "url": vacancy.url,
+                                 "description": vacancy.description,
+                                 "requirements": vacancy.requirements})
 
             json.dump(all_data, file, ensure_ascii=False)
 
-    def get_data(self, param: str) -> list:
+    def get_vacancy(self, name_vacancy: str) -> dict:
         """Метод для получения данных из json файла по параметрам"""
-        sorted_data = []
-
         with open(os.path.join("../", "data", "data.json"), "r", encoding="utf-8") as file:
-            data = json.load(file)
+            vacancies = json.load(file)
 
-            for vacancy in data:
-                if param in vacancy.values():
-                    sorted_data.append(vacancy)
+            for vacancy in vacancies:
+                if name_vacancy in vacancy["name"]:
+                    return vacancy
 
-        return sorted_data
-
-    def delete_data(self) -> None:
+    def delete_vacancies(self) -> None:
         """Метод для удаления данных из json файла"""
         with open(os.path.join("../", "data", "data.json"), "w", encoding="utf-8") as file:
             file.write("")
